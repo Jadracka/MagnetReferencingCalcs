Fixed Rodrigues' rotation
Fixed the rotation of the plane to XY plane by writing own algorithm following: https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula.
Current version functions and provides correct results, highly likely in general cases.

main.py
@@ -1,24 +1,37 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Jul 13 09:40:21 2023
 
 @author: jbarker
 """
 
+
+
 import functions as fc
 import config as cg
+import numpy as np
 
 
-#keyence3d = fc.fit_circle_3D(fc.read_data_from_file(cg.keyence_path), cg.output_units, cg.point_transform_check_tolerance)
-LT_out = fc.fit_circle_3D(fc.read_data_from_file(cg.LT_path), 
-                          cg.output_units_LT,
-                          cg.point_transform_check_tolerance)
+# keyence3d = fc.fit_circle_3D(fc.read_data_from_file(cg.keyence_path),
+#                        cg.output_units, cg.point_transform_check_tolerance)
+# LT_out = fc.fit_circle_3D(fc.read_data_from_file(cg.LT_path),
+#                          cg.output_units_LT,
+#                          cg.point_transform_check_tolerance)
 #plane_coefficients = fc.fit_plane(fc.read_data_from_file(cg.LT_path), cg.output_units_LT, cg.log_statistics)
 
 #data_dictLT, file_pathLT = fc.read_data_from_file(cg.LT_path)
 #testing_plane_coefficients, testing_plane_statistics = fc.fit_plane(fc.read_data_from_file(cg.planeANDcircle_test_points_path), cg.output_units, cg.log_statistics)
 #test_plane1, test_plane1_statistics = fc.fit_plane(fc.read_data_from_file(cg.test_plane1_path), cg.output_units, cg.log_statistics)
-#testing_circle_coefficients = fc.fit_circle_3D(
-#        fc.read_data_from_file(cg.planeANDcircle_test_points_path), 
-#        cg.output_units_LT,
-#        cg.point_transform_check_tolerance)
+testing_circle_coefficients = fc.fit_circle_3D(
+        fc.read_data_from_file(cg.planeANDcircle_test_points_path),
+        cg.output_units_LT,
+        cg.point_transform_check_tolerance)
+
+
+
+
+
+
+v1 = np.array([0, 0, 1])
+v2 = np.array([0, 1, 0])
+rotation_matrix = fc.rotation_matrix_from_vectors(v1, v2)
+#print(rotation_matrix)